7c032a288115b4476255d7d430c92022ec06dc3c
Hanson Lee <icycandice@gmail.com>
2024-06-09 00:12:20 -0700

    Implemented Double Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private bool isGrounded;
+    private bool canDoubleJump;
     private Rigidbody2D rb;
     private float xMovementInput;
 
@@ -22,9 +23,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Jump();
         CollisionCheck();
         xMovementInput = Input.GetAxis("Horizontal");
-        Jump();
+        
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
     }
 
 
@@ -38,7 +45,7 @@ public class Player : MonoBehaviour
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
     }
-    
+
     private void Move()
     {
         rb.velocity = new Vector2(moveSpeed * xMovementInput, rb.velocity.y);
@@ -48,6 +55,11 @@ public class Player : MonoBehaviour
     {
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.Space)) && isGrounded)
         {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+        else if (canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            canDoubleJump = false;
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
     }
